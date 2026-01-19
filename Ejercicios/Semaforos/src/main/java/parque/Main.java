6ccd764d3f0966b7c25f7d2061a23535cb997600
Alberto Bollo <albertobollo7@protonmail.com>
2026-01-19 23:50:49 +0100

    chore: Ejercicio PSP parque

---
+++ Ejercicios/Semaforos/src/main/java/parque/Main.java
@@ -0,0 +1,14 @@
+package parque;
+/*
+Considérese el caso de un parque público que dispone de cinco puertas de acceso.
+El acceso por cada una de las puertas del parque, está controlada por un torno independiente,
+que envía un evento propio a una aplicación de computador que debe contarlas
+y proporcionar en cualquier instante el número total de visitantes que han entrado en el parque.
+Cuando la actividad de los tornos ha concluido proporciona
+la información sobre el número de visitantes que se han producido en total y de cada torno.
+ */
+public class Main {
+    public static void main (String[] args){
+
+    }
+}
